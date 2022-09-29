eb2dc176fcd3d2ffcb260fa17ef88aadd84d397a
Leonardo Rebouças Batista <reboucas.beraby@gmail.com>
2022-09-29 01:56:45 -0300

    chore: add project item

--- src/pages/PortfolioItem/PortfolioItem.tsx
+++ src/pages/PortfolioItem/PortfolioItem.tsx
@@ -1,21 +1,23 @@
 //** External Imports */
 import React from "react";
+import { Link, useLoaderData } from "react-router-dom";
 
 import Box from "@mui/material/Box";
-import Stack from "@mui/material/Stack";
-import Typography from "@mui/material/Typography";
 import Breadcrumbs from "@mui/material/Breadcrumbs";
 import MuiLink from "@mui/material/Link";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
 
-import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import FacebookIcon from "@mui/icons-material/Facebook";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import TwitterIcon from "@mui/icons-material/Twitter";
 
-import { Link, useLoaderData } from "react-router-dom";
-import LinkedinShareButton from "react-share/lib/LinkedinShareButton";
 import FacebookShareButton from "react-share/lib/FacebookShareButton";
+import LinkedinShareButton from "react-share/lib/LinkedinShareButton";
 import TwitterShareButton from "react-share/lib/TwitterShareButton";
+
 //** Local Imports */
+import { withScrollToTop } from "hoc";
 
 //** Typings */
 export interface PortfolioItemProps {}
@@ -91,93 +93,105 @@ const PortfolioItem: React.FC<PortfolioItemProps> = () => {
         </Breadcrumbs>
       </Stack>
 
-      <Box
-        display="grid"
-        gridTemplateColumns={{
-          // tablet: "repeat(auto-fill, minmax(300px, 1fr))",
-          mobile: "repeat(auto-fill, minmax(200px, 1fr))",
-        }}
-        height="min-content"
-        gap="1rem"
-        padding={{ tabletLarge: "0 10rem", mobile: "3rem" }}
-      >
-        {project.imgs.map((img: string, index: number) => (
-          <Box key={index} height="100%" width="100%">
-            <img
-              src={img}
-              style={{ objectFit: "cover", width: "100%" }}
-              alt="project sample"
-            />
-          </Box>
-        ))}
-      </Box>
-
       <Stack
+        direction={{ laptop: "row" }}
         padding={{ tabletLarge: "0 10rem", mobile: "0 3rem" }}
-        gap="3rem"
-        mb="3rem"
+        mt="5rem"
+        gap="5rem"
+        mb="5rem"
       >
-        <Typography variant="topic">{project.details}</Typography>
-        <Typography variant="content">{project.description}</Typography>
-      </Stack>
-
-      <Stack padding={{ tabletLarge: "0 10rem", mobile: "0 3rem" }} mb="3rem">
-        <Stack direction="row" gap="0.5rem">
-          <Typography variant="section">Date:</Typography>
-          <Typography
-            variant="content"
-            sx={{
-              "&:hover": {
-                color: (theme) => theme.palette.primary.main,
-              },
-            }}
-          >
-            {project.date}
-          </Typography>
-        </Stack>
-        <Stack direction="row" gap="0.5rem">
-          <Typography variant="section">Tags:</Typography>
-          {project.tags.map((tag: any, index: number) => (
-            <Typography
-              component={Link}
-              sx={{
-                textDecoration: "none",
-                color: "black",
-                "&:hover, &:focus": { color: "primary.main" },
-              }}
-              to="/portfolio"
-              state={{ tag: tag.tag }}
-            >
-              {tag.pretty}
-              {index + 1 === project?.tags?.length ? "" : ","}
-            </Typography>
+        <Box
+          display="grid"
+          gridTemplateColumns={{
+            laptop: "repeat(2, minmax(250px, 1fr))",
+            mobile: "repeat(auto-fill, minmax(300px, 1fr))",
+          }}
+          height="min-content"
+          gap="1rem"
+        >
+          {project.imgs.map((img: string, index: number) => (
+            <Box key={index} height="100%" width="100%">
+              <img
+                src={img}
+                style={{ objectFit: "cover", width: "100%" }}
+                alt="project sample"
+              />
+            </Box>
           ))}
-        </Stack>
-        <Stack direction="row" gap="0.5rem" alignItems="center">
-          <Typography variant="section">Share:</Typography>
-          <FacebookShareButton
-            title={project.details}
-            quote={project.description}
-            url={`${process.env.REACT_APP_LIVE_URL}/portfolio/${project.id}`}
-            style={{ display: "flex" }}
-          >
-            <FacebookIcon />
-          </FacebookShareButton>
-          <TwitterShareButton
-            title={project.details}
-            url={`${process.env.REACT_APP_LIVE_URL}/portfolio/${project.id}`}
-            style={{ display: "flex" }}
-          >
-            <TwitterIcon />
-          </TwitterShareButton>
-          <LinkedinShareButton
-            title={project.details}
-            summary={project.description}
-            url={`${process.env.REACT_APP_LIVE_URL}/portfolio/${project.id}`}
-            style={{ display: "flex" }}
-          >
-            <LinkedInIcon />
-          </LinkedinShareButton>
+        </Box>
+
+        <Stack gap="3rem" mb="3rem" mt="3rem">
+          <Typography variant="topic">{project.details}</Typography>
+          <Typography variant="content">{project.description}</Typography>
+
+          <Stack gap="0.5rem">
+            <Stack direction="row" gap="0.5rem">
+              <Typography variant="section">Date:</Typography>
+              <Typography
+                variant="content"
+                sx={{
+                  "&:hover": {
+                    color: (theme) => theme.palette.primary.main,
+                  },
+                }}
+              >
+                {project.date}
+              </Typography>
+            </Stack>
+            <Stack direction="row" gap="0.5rem">
+              <Typography variant="section">Tags:</Typography>
+              {project.tags.map((tag: any, index: number) => (
+                <Typography
+                  component={Link}
+                  sx={{
+                    textDecoration: "none",
+                    color: "black",
+                    "&:hover, &:focus": { color: "primary.main" },
+                  }}
+                  to="/portfolio"
+                  state={{ tag: tag.tag }}
+                >
+                  {tag.pretty}
+                  {index + 1 === project?.tags?.length ? "" : ","}
+                </Typography>
+              ))}
+            </Stack>
+            <Stack direction="row" gap="0.5rem" alignItems="center">
+              <Typography variant="section">Share:</Typography>
+              <FacebookShareButton
+                title={project.details}
+                quote={project.description}
+                url={`${process.env.REACT_APP_LIVE_URL}/portfolio/${project.id}`}
+                style={{ display: "flex" }}
+              >
+                <FacebookIcon
+                  color="secondary"
+                  sx={{ "&:hover, &:focus": { color: "primary.main" } }}
+                />
+              </FacebookShareButton>
+              <TwitterShareButton
+                title={project.details}
+                url={`${process.env.REACT_APP_LIVE_URL}/portfolio/${project.id}`}
+                style={{ display: "flex" }}
+              >
+                <TwitterIcon
+                  color="secondary"
+                  sx={{ "&:hover, &:focus": { color: "primary.main" } }}
+                />
+              </TwitterShareButton>
+              <LinkedinShareButton
+                title={project.details}
+                summary={project.description}
+                url={`${process.env.REACT_APP_LIVE_URL}/portfolio/${project.id}`}
+                style={{ display: "flex" }}
+              >
+                <LinkedInIcon
+                  color="secondary"
+                  sx={{ "&:hover, &:focus": { color: "primary.main" } }}
+                />
+              </LinkedinShareButton>
+            </Stack>
+          </Stack>
         </Stack>
       </Stack>
     </Stack>
@@ -186,4 +200,4 @@ const PortfolioItem: React.FC<PortfolioItemProps> = () => {
 
 PortfolioItem.defaultProps = defaultProps;
 
-export default PortfolioItem;
+export default withScrollToTop(PortfolioItem);
