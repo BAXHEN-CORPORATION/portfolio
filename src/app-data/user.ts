e0919aaf561925e84f123fe276e3e2e748812d65
Leonardo Rebouças Batista <reboucas.beraby@gmail.com>
2022-09-30 13:42:42 -0300

    feat: add thanks pages

--- src/app-data/user.ts
+++ src/app-data/user.ts
@@ -1,7 +1,7 @@
 export const user = {
   name: "Maria da Silva",
   phone: "+55 (99) 99999-9999",
-  email: "maria@maria.com",
+  email: "marias@maria.com",
   resumeFilename: "resume.pdf",
   description:
     "Sempre fui apaixona em mostrar e expressar o que tem de mais bonito em obras de artes e designs. Então me descobri sendo uma designer web de coração desde pequena. Desde então venho me aperfeiçoando em como mostrar o que tem de melhor nos produtos, marcas e empresas.",
