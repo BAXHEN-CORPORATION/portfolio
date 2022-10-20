de133f4f1eb06c974b9c0bdba3e20f97eca2bdab
Leonardo Rebouças Batista <reboucas.beraby@gmail.com>
2022-10-20 19:21:51 -0300

    remove hydration

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { hydrate } from "react-dom";
 
 const rootElement = document.getElementById("root") as HTMLElement;
 const root = ReactDOM.createRoot(rootElement);
 
-if (rootElement.hasChildNodes()) {
-  hydrate(<App />, rootElement);
-} else {
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-}
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
